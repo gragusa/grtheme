1ea07ccef8886bc845bb9e2c0a417dc6a7e2e4b2
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-02-15 01:02:00 +0100

    Changed colour

--- R/grtheme.R
+++ R/grtheme.R
@@ -21,9 +21,9 @@ theme_gr <- function(base_size = 12, base_family = "sans") {
      legend.box = "vertical",
      plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
      plot.margin = unit(c(1, 1, 1, 1), "lines"),
-     strip.background=element_rect(fill = "#43609C", colour = "slategray3"),
-     strip.text.x = element_text(colour = "white"),
-     strip.text.y = element_text(colour = "white")
+     strip.background=element_rect(fill = "#D2D2D2", colour = "#D2D2D2"),
+     strip.text.x = element_text(colour = "#ec872a"),
+     strip.text.y = element_text(colour = "#ec872a")
     ))
 }
 
